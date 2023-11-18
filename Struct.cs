42337b6b1be7bf417cf183cefdcc7668af798768
nikitos <nikitos@DESKTOP-0OKDFES>
2023-11-18 18:26:08 +0300

    Добавьте файлы проекта.

---
+++ Struct.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructType
+{
+    public enum AccountType { Checking, Deposit }
+    public struct BankAccount
+    {
+        public long accNo;
+        public decimal accBal;
+        public AccountType AccType;
+    }
+    internal class Struct
+    {
+        static void Main(string[] args)
+        {
+            BankAccount goldAccount;
+            goldAccount.AccType = AccountType.Checking;
+            goldAccount.accBal = (decimal) 3200.00;
+            Console.Write("Enter account number: ");
+            goldAccount.accNo = long.Parse(Console.ReadLine());
+            //goldAccount.accNo = 1234;
+            Console.WriteLine("Account Number {0}", goldAccount.accNo);
+            Console.WriteLine("Account Type {0}", goldAccount.AccType);
+            Console.WriteLine("Account Balance ${0}", goldAccount.accBal);
+            Console.ReadLine();
+        }
+    }
+}
